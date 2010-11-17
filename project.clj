bb883ac1a4a05592b643928139b5942458aa2b2b
Phil Hagelberg <technomancy@gmail.com>
2010-11-17 11:28:37 -0800

    Bump version number to 0.2.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-stacktrace "0.2.0"
+(defproject clj-stacktrace "0.2.1-SNAPSHOT"
   :description "More readable stacktraces in Clojure programs."
   :url "http://github.com/mmcgrana/clj-stacktrace"
   :dependencies [[org.clojure/clojure "1.2.0"]]
