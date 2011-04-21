507a0e0c091bb3c6885d81d9588d43ee0d6ee498
Joe Gallo <joegallo@gmail.com>
2011-04-21 09:58:45 -0400

    Update clojure dependency, and make it a dev-dependency.
    That way other projects don't have end up pulling in clj-stacktrace's
    preferred clojure version, and can use whatever they please.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-stacktrace "0.2.1-SNAPSHOT"
   :description "More readable stacktraces in Clojure programs."
   :url "http://github.com/mmcgrana/clj-stacktrace"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.6.0"]])
+  :dev-dependencies [[org.clojure/clojure "1.2.1"]
+                     [lein-clojars "0.6.0"]])
