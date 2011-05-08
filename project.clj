6fd01866c1d7e3bf8d59ef2d7f05aef396d0b811
Mark McGranaghan <mmcgrana@gmail.com>
2011-05-07 17:47:05 -0700

    Version 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-stacktrace "0.2.1-SNAPSHOT"
+(defproject clj-stacktrace "0.2.2"
   :description "More readable stacktraces in Clojure programs."
   :url "http://github.com/mmcgrana/clj-stacktrace"
   :dev-dependencies [[org.clojure/clojure "1.2.1"]
