852428afd5436121ae0f8d890d81e0533f4fa224
Phil Hagelberg <technomancy@gmail.com>
2011-07-25 15:21:22 -0700

    Bump to version 0.2.3.

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject clj-stacktrace "0.2.3-SNAPSHOT"
+(defproject clj-stacktrace "0.2.3"
   :description "More readable stacktraces in Clojure programs."
   :url "http://github.com/mmcgrana/clj-stacktrace"
   :dev-dependencies [[org.clojure/clojure "1.2.1"]
-                     [lein-clojars "0.6.0"]]
-  :hooks [leiningen.hooks.clj-stacktrace-test]
-  :repl-options [:caught clj-stacktrace.repl/pst+])
+                     [lein-clojars "0.6.0"]])
