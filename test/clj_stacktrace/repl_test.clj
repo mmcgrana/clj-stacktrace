735b7165d0de15d4f4fea59319ede15e605d6913
Alex Baranosky <alexander.baranosky@gmail.com>
2011-12-31 19:46:45 -0500

    added ability to configure which colors clj-stacktrace uses

--- test/clj_stacktrace/repl_test.clj
+++ test/clj_stacktrace/repl_test.clj
@@ -1,7 +1,8 @@
 (ns clj-stacktrace.repl-test
-  (:use clojure.test)
-  (:use clj-stacktrace.utils)
-  (:use clj-stacktrace.repl))
+  (:use clojure.test
+        clj-stacktrace.utils
+        clj-stacktrace.repl
+        midje.sweet))
 
 (defmacro with-cascading-exception
   "Execute body in the context of a variable bound to an exception instance
@@ -29,3 +30,35 @@
     (is (with-out-str (pst+ e)))
     (binding [*e e]
       (is (with-out-str (pst+))))))
+
+;; Color configuration tests
+
+(defn- starts-colored [color]
+  #(.startsWith % (color-codes color)) )
+
+(fact "defaults to red errors" 
+  (with-cascading-exception ex 
+    (with-out-str (pst+ ex))) => (starts-colored :red))
+
+(against-background [(around :contents (do
+                                         (configure-colors {:error-color :blue})
+                                         ?form
+                                         (reset-color-configuration)))]
+  (fact "colors are configurable"
+    (with-cascading-exception ex 
+      (with-out-str (pst+ ex))) => (starts-colored :blue)))
+
+(fact "then, can reset the colors to defaults"
+  (with-cascading-exception ex 
+    (with-out-str (pst+ ex))) => (starts-colored :red))
+
+;; Valid configuration?
+
+(fact "must pass valid config keys"
+  (configure-colors {:ERROR-COLOR :red}) => (throws Exception)
+  (configure-colors {:error-color :red}) =not=> (throws Exception))
+
+(fact "must pass valid config values"
+  (configure-colors {:error-color :purple}) => (throws Exception)
+  (configure-colors {:error-color :red}) =not=> (throws Exception))
+
