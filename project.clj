0c86086bd19d9b11c02f61a26b432faf06e727c9
Phil Hagelberg <technomancy@gmail.com>
2013-06-20 13:59:42 -0700

    Bump to 0.2.6.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-stacktrace "0.2.5"
+(defproject clj-stacktrace "0.2.6"
   :description "More readable stacktraces in Clojure programs."
   :url "http://github.com/mmcgrana/clj-stacktrace"
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
