879073093139e763f0750a8cf021492a48e54d1a
Phil Hagelberg <technomancy@gmail.com>
2014-05-22 11:59:02 -0700

    Release 0.2.8.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-stacktrace "0.2.7"
+(defproject clj-stacktrace "0.2.8"
   :description "More readable stacktraces in Clojure programs."
   :url "https://github.com/mmcgrana/clj-stacktrace"
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
